9fbc5df6bd1320d50c9b376ca18b8971e7ef142c
Othon Oliveira <othonoliveira08@gmail.com>
2022-10-31 18:56:55 -0300

    Teste

---
+++ src/tests/requirement01.spec.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import { act } from 'react-dom/test-utils';
+// import renderWithRouter from '../renderWithRouter';
+// import App from '../App';
+
+describe('', () => {
+  it('', () => {
+  });
+});
